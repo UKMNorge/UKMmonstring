dd2750a8313fc503f0fb83d0d5fc5a58334eb3a3
Marius Mandal <mariusmandal@gmail.com>
2013-10-07 17:33:41 +0200

    bugfix order + cleanup

--- ukmmonstring.php
+++ ukmmonstring.php
@@ -44,8 +44,11 @@ function UKMMonstring() {
 
 function UKMmonstring_save_kontaktpersoner() {
 	$pl_id = get_option('pl_id');
+	
+	$i = 0;
 
 	foreach($_POST['order'] as $contact) {
+		$i++;
 		$id = (int) $contact;
 		if(empty($id))
 			continue;
@@ -54,82 +57,7 @@ function UKMmonstring_save_kontaktpersoner() {
 						  array('pl_id'=>$pl_id,
 						  		'ab_id'=>$id));
 		$qry->add('order', $i);
-		//$res = $qry->run();
-		echo $qry->debug();
+		$res = $qry->run();
 	}
 	die();
 }
-
-/*
-function UKMMonstring_allow(){
-	$plid = (int)get_option('pl_id');
-	$type = (int)$_POST['type'];
-	if($plid == 0 || $type == 0)
-		die('Ugyldig input!');
-	
-	if($_POST['setting'] == 'true') {
-		$del = new SQLdel('smartukm_rel_pl_bt', array('pl_id'=>$plid, 'bt_id'=>$type));
-		$delRes = $del->run();
-			
-		$ins = new SQLins('smartukm_rel_pl_bt');
-		$ins->add('pl_id', $plid);
-		$ins->add('bt_id', $type);
-		$ins->run();
-
-		UKMlog('smartukm_rel_pl_bt','bt'.$type, 'setting');
-		
-		die();
-	} else {
-		$del = new SQLdel('smartukm_rel_pl_bt', array('pl_id'=>$plid, 'bt_id'=>$type));
-		$delRes = $del->run();
-		
-		UKMlog('smartukm_rel_pl_bt','bt'.$type, 'setting');
-	}
-	die();
-}
-*/
-
-
-/*
-	
-	if(isset($_GET['contact'])) {
-		require_once('UKM/kontakt.class.php');
-		require_once('monstring.contact.php');
-		echo contactForm();
-		return;
-	}
-	
-	UKM_loader('form|toolkit');
-	require_once('monstring.form.php');
-	require_once('monstring.lang.php');
-	require_once('monstring.save.php');
-
-	echo '<div class="wrap">'.UKMMonstring_form($place, $user).'</div>';
-
-	
-}
-*/
-/*
-
-function my_admin_scripts() {
-	wp_enqueue_script('media-upload');
-	wp_enqueue_script('thickbox');
-	wp_register_script('UKMcontactImageupload', plugin_dir_url( __FILE__ ).'monstring.image.js', array('jquery','media-upload','thickbox'));
-	wp_enqueue_script('UKMcontactImageupload');
-}
-
-function my_admin_styles() {
-	wp_enqueue_style('thickbox');
-}
-
-if(isset($_GET['contact'])) {
-	add_action('admin_print_scripts', 'my_admin_scripts');
-	add_action('admin_print_styles', 'my_admin_styles');
-}
-function filter_iste($html, $id, $caption, $title, $align, $url, $size, $alt) {
-	var_dump($html);
-    return $html;
-}
-
-
-*/
