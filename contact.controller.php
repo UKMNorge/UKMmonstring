5f19aef743ca20342cbb281bb1e81becee15697e
Marius Mandal <mariusmandal@gmail.com>
2013-10-08 09:54:34 +0200

    contact controller

--- contact.controller.php
+++ contact.controller.php
@@ -1,2 +1,27 @@
 <?php 
+
+if(is_numeric($_GET['contact'])) {
+	$contact = new kontakt($_GET['contact']);
+	
+	$infos = array('id'		=> $contact->get('id'),
+				   'firstname' = $contact->get('firstname'),
+				   'lastname' = $contact->get('lastname'),
+				   'phone' = $contact->get('tlf'),
+				   'email' = $contact->get('email'),
+				   'title' = $contact->get('title'),
+				   'facebook' = $contact->get('facebook'),
+				   'kommune' = $contact->get('kommune'),
+				   'address' = $contact->get('address'),
+				   'postal' = $contact->get('postalcode'),
+				   )
+} else {
+	$infos['id'] = 'new';
+}
+
+$pl = new monstring(get_option('pl_id'));
+
+$infos['kommuner'] = $pl->kommuner();
+$infos['is_fellesmonstring'] = $pl->fellesmonstring();
+$infos['site_type']	= get_option('site_type');
+
 ?>
