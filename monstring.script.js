0e2f9b9d72dbd2bf53761401a3f92166d3feeb6e
Marius Mandal <mariusmandal@gmail.com>
2013-10-08 10:35:14 +0200

    load sequence

--- monstring.script.js
+++ monstring.script.js
@@ -48,26 +48,31 @@ jQuery(document).ready(function(){
 		});
 	}});
 	jQuery( "#kontaktpersoner" ).disableSelection();
+
+
+	jQuery('#imageedit').click(function() {
+		console.log('Bytt bilde');
+	    var send_attachment_bkp = wp.media.editor.send.attachment;
+	
+	    wp.media.editor.send.attachment = function(props, attachment) {
+	
+	        jQuery('#custom_media_image').attr('src', attachment.url);
+	        jQuery('#custom_media_url').val(attachment.url);
+	        jQuery('#custom_media_id').val(attachment.id);
+	
+	        wp.media.editor.send.attachment = send_attachment_bkp;
+	    }
+	
+	    wp.media.editor.open();
+	
+	    return false;       
+	});
+
+
+
 });
 
 
 
 
 
-jQuery('#imageedit').click(function() {
-	alert('fix bilde');
-    var send_attachment_bkp = wp.media.editor.send.attachment;
-
-    wp.media.editor.send.attachment = function(props, attachment) {
-
-        jQuery('#custom_media_image').attr('src', attachment.url);
-        jQuery('#custom_media_url').val(attachment.url);
-        jQuery('#custom_media_id').val(attachment.id);
-
-        wp.media.editor.send.attachment = send_attachment_bkp;
-    }
-
-    wp.media.editor.open();
-
-    return false;       
-});
