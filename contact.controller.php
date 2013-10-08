036372760bc46675f25e10323bab9b907c9b35c3
Marius Mandal <mariusmandal@gmail.com>
2013-10-08 09:55:04 +0200

    array info

--- contact.controller.php
+++ contact.controller.php
@@ -4,15 +4,15 @@ if(is_numeric($_GET['contact'])) {
 	$contact = new kontakt($_GET['contact']);
 	
 	$infos = array('id'		=> $contact->get('id'),
-				   'firstname' = $contact->get('firstname'),
-				   'lastname' = $contact->get('lastname'),
-				   'phone' = $contact->get('tlf'),
-				   'email' = $contact->get('email'),
-				   'title' = $contact->get('title'),
-				   'facebook' = $contact->get('facebook'),
-				   'kommune' = $contact->get('kommune'),
-				   'address' = $contact->get('address'),
-				   'postal' = $contact->get('postalcode'),
+				   'firstname' => $contact->get('firstname'),
+				   'lastname' => $contact->get('lastname'),
+				   'phone' => $contact->get('tlf'),
+				   'email' => $contact->get('email'),
+				   'title' => $contact->get('title'),
+				   'facebook' => $contact->get('facebook'),
+				   'kommune' => $contact->get('kommune'),
+				   'address' => $contact->get('address'),
+				   'postal' => $contact->get('postalcode'),
 				   )
 } else {
 	$infos['id'] = 'new';
