45bd19ea3ac675427661f7b3c75387e14e490cc4
Marius Mandal <mariusmandal@gmail.com>
2018-09-13 19:00:59 +0200

    Lagt ut info om videresendingsskjema

--- ukmmonstring.php
+++ ukmmonstring.php
@@ -93,11 +93,11 @@ function UKMMonstring() {
     require_once('UKM/monstring.class.php');
     $monstring = new monstring_v2( get_option('pl_id') );
 
-	if( date('y') > 6 && (int)$monstring->getSesong() <= (int)date('Y') ) {
+	if( false ){# date('y') > 6 && (int)$monstring->getSesong() <= (int)date('Y') ) {
 		echo TWIG('vent-til-ny-sesong.html.twig', [], dirname(__FILE__));
 		return;
 	}
-	
+
 	$TWIGdata = [];
 	$CONTROLLER = isset( $_GET['kontakt'] ) ? 'kontakt' : 'monstring';	
 	// Might modify $CONTROLLER
