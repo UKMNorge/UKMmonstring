7815aef3da460ec5932a6bb2665a247f8bb4f25f
Marius Mandal <mariusmandal@gmail.com>
2019-08-29 15:48:09 +0200

    Datepicker-conf flyttet til UKMresources

--- monstring.script.js
+++ monstring.script.js
@@ -1,31 +1,3 @@
-jQuery(document).ready(function(){
-/* DATEPICKER */
-	jQuery( ".datepicker_kommune" ).datepicker( {minDate: new Date(UKMSEASON,0,1), maxDate: new Date(UKMSEASON, 2, 31), dateFormat: 'dd.mm.yy'});
-	jQuery( ".datepicker_fylke" ).datepicker( {minDate: new Date(UKMSEASON,2,1), maxDate: new Date(UKMSEASON, 3, 30), dateFormat: 'dd.mm.yy'});
-	jQuery( ".datepicker_fylke_forward" ).datepicker( {minDate: new Date(UKMSEASON,1,1), maxDate: new Date(UKMSEASON, 3, 30), dateFormat: 'dd.mm.yy'});
-	jQuery( ".datepicker_land" ).datepicker( {minDate: new Date(UKMSEASON,4,1), maxDate: new Date(UKMSEASON, 6, 31), dateFormat: 'dd.mm.yy'});
-
-    jQuery.datepicker.regional['no'] = {
-		closeText: 'Lukk',
-        prevText: '&laquo;Forrige',
-		nextText: 'Neste&raquo;',
-		currentText: 'I dag',
-        monthNames: ['Januar','Februar','Mars','April','Mai','Juni',
-        'Juli','August','September','Oktober','November','Desember'],
-        monthNamesShort: ['Jan','Feb','Mar','Apr','Mai','Jun',
-        'Jul','Aug','Sep','Okt','Nov','Des'],
-		dayNamesShort: ['S&oslash;n','Man','Tir','Ons','Tor','Fre','L&oslash;r'],
-		dayNames: ['S&oslash;ndag','Mandag','Tirsdag','Onsdag','Torsdag','Fredag','L&oslash;rdag'],
-		dayNamesMin: ['S&oslash;','Ma','Ti','On','To','Fr','L&oslash;'],
-		weekHeader: 'Uke',
-        dateFormat: 'yy-mm-dd',
-		firstDay: 1,
-		isRTL: false,
-		showMonthAfterYear: false,
-		yearSuffix: ''};
-    jQuery.datepicker.setDefaults(jQuery.datepicker.regional['no']);
-});
-
 jQuery(document).ready( function(){
     jQuery( "#kontaktpersoner" ).sortable(
         {
