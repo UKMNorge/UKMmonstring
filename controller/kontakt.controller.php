9350ed2dc411579839d26abcb42beb666cde44f7
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2024-12-20 16:23:34 +0100

    Revert "Redigering av OmradeKontaktperson"
    This reverts commit bda6c541f82ebe57c18af8962ca2b4d0cf773f69.

--- controller/kontakt.controller.php
+++ controller/kontakt.controller.php
@@ -1,78 +1,27 @@
 <?php
 
-use UKMNorge\Arrangement\Arrangement;
-use UKMNorge\OAuth2\ArrSys\HandleAPICallWithAuthorization;
-use UKMNorge\Nettverk\Omrade;
-use UKMNorge\Nettverk\OmradeKontaktperson;
-use UKMNorge\Nettverk\WriteOmradeKontaktperson;
-use UKMNorge\Nettverk\OmradeKontaktpersoner;
-use UKMNorge\OAuth2\ArrSys\AccessControlArrSys;
+use UKMNorge\Arrangement\Kontaktperson\Kontaktperson;
 
 require_once('UKM/Autoloader.php');
 
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $arrangement = new Arrangement(intval((get_option('pl_id'))));
-    $omrade = $arrangement->getEierOmrade();
-
-    $tilgang = 'kommune_eller_fylke'; // Har tilgang til kommunen eller fylket
-    $tilgangAttribute = $omrade->getForeignId();
-
-    $handleCall = new HandleAPICallWithAuthorization(['okpId', 'fornavn', 'mobil', 'etternavn', 'epost'], ['beskrivelse', 'deletedProfileImage'], ['POST'], false, false, $tilgang, $tilgangAttribute);
-
-    $id = $handleCall->getArgument('okpId');
-    $fornavn = $handleCall->getArgument('fornavn');
-    $etternavn = $handleCall->getArgument('etternavn');
-    $epost = $handleCall->getArgument('epost');
-    $mobil = $handleCall->getArgument('mobil');
-    $beskrivelse = $handleCall->getOptionalArgument('beskrivelse') ?? '';
-    $deletedProfileImage = $handleCall->getOptionalArgument('deletedProfileImage') == 'true' ? true : false;
-    
-    // Check mobil
-    if(!preg_match('/^\d{8}$/', $mobil)) {
-        HandleAPICallWithAuthorization::sendError('Mobilnummeret må være 8 siffer og kun tall', 400);
-    }
-
-    try {
-        $okp = OmradeKontaktpersoner::getByMobil($mobil);
-        $okp->setFornavn($fornavn);
-        $okp->setEtternavn($etternavn);
-        $okp->setEpost($epost);
-        $okp->setBeskrivelse($beskrivelse);
-        
-        WriteOmradeKontaktperson::uploadProfileImage($_FILES['profile_picture'], $okp, $deletedProfileImage);
-        WriteOmradeKontaktperson::editOmradekontaktperson($okp);
-    } catch(Exception $e) {
-        HandleAPICallWithAuthorization::sendError($e->getMessage(), 400);
-    }
-
-    echo '<script>window.location.href = "?";</script>';
-    exit();
-}
-else {
-    $okpId = HandleAPICallWithAuthorization::getArgumentBeforeInit('okpId', 'GET');
-    $okp = OmradeKontaktpersoner::getById($okpId);
-
-    $omrade = new Omrade($okp->getEierOmradeType(), $okp->getEierOmradeId());
-    if(!AccessControlArrSys::hasOmradeAccess($omrade)) {
-        UKMnettverket::addViewData('omrade', $omrade);
-        UKMnettverket::addViewData('tilgang', false);
-    }
-    else {
-        showUser($okp);
-    }
-}
-
-function showUser(OmradeKontaktperson $okp) {
-    $omrade = new Omrade($okp->getEierOmradeType(), $okp->getEierOmradeId());
-
-    UKMnettverket::addViewData('tilgang', true);
-    UKMnettverket::addViewData('omradekontaktperson', $okp);
-    UKMnettside::addViewData('omrade', $omrade);
-
+if( is_numeric( $_GET['kontakt'] ) ) {
+    $kontakt = new Kontaktperson( intval($_GET['kontakt']) );
     UKMmonstring::addViewData(
         'kontakt',
-        $okp
+        $kontakt
     );
-
 }
+
+$arrangement = UKMmonstring::getArrangement();
+
+$admin_kontakter = [];
+foreach( $arrangement->getKontaktpersoner()->getAll() as $kontakt ) {
+    if( is_numeric( $kontakt->getAdminId() ) ) {
+        $admin_kontakter[] = $kontakt->getAdminId();
+    }
+}
+
+UKMmonstring::addViewData(
+    'admin_kontakter', 
+    $admin_kontakter
+);
